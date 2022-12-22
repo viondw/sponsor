f4a3898840573f1db0e9733ce714237f5101e05d
miloce <admim@luozhinet.com>
2022-12-22 23:06:09 +0800

    CI

---
+++ README.md
@@ -0,0 +1,45 @@
+# afdian-sponsor-page-vercel
+
+一个独立展示爱发电赞助用户的网页，但在Vercel上部署。
+
+## 特点
+
+~~...我想不出来233~~明明有很多可写的嘛...
+
+采用Material Design设计
+
+基于PHP的动态页面
+
+轻量级页面实现快速加载
+
+基于Vercel部署Serverless服务，无需服务器费用支出
+
+## 使用效果
+
+访问 https://afdian-sponsor-page-vercel.vercel.app/ 来查看
+
+## 使用方法
+
+1. 在Vercel上部署此仓库
+
+采用此Fork版本库：
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Flwd-temp%2Fafdian-sponsor-page-vercel%2Ftree%2Fvercel&env=USERID,TOKEN,PAGETITLE,USERNAME)
+
+或者采用上游仓库的对应分支：
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FMisaLiu%2Fafdian-sponsor-page%2Ftree%2Fvercel&env=USERID,TOKEN,PAGETITLE,USERNAME)
+
+2. 获取你的爱发电 UserId 和 API Token [点此一键直达](https://afdian.net/dashboard/dev)
+
+3. 将这些信息填入环境变量 USERID（UserId） TOKEN（API Token）
+
+4. 填写其他环境变量 PAGETITLE（网页标题） USERNAME（你的用户名，即你的主页地址 @ 后面的那部分）
+
+5. 访问一下试试吧！
+
+## 鸣谢
+* [juicyfx/vercel-php](https://github.com/juicyfx/vercel-php)
+* [MisaLiu/afdian-sponsor-page](https://github.com/MisaLiu/afdian-sponsor-page)
+* [zdhxiong/mdui](https://github.com/zdhxiong/mdui)
+* [爱发电](https://afdian.net)
---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+function switchPage (pageId) {
+    mdui.$.ajax({
+        method: 'POST',
+        url: 'index.php',
+        data: {
+            page: pageId
+        },
+        start: function () {
+            mdui.$('#mdui_progress').removeClass('mdui-hidden');
+            mdui.$.showOverlay();
+            mdui.$.lockScreen();
+        },
+        complete: function () {
+            mdui.$('#mdui_progress').addClass('mdui-hidden');
+            mdui.$.hideOverlay();
+            mdui.$.unlockScreen();
+        },
+        success: function (data) {
+            let _json = undefined;
+
+            try {
+                _json = JSON.parse(data);
+            } catch (e) {
+                mdui.alert('拉取信息时出错', '出错啦！');
+                return;
+            }
+
+            if (_json.code == 200) {
+                if (_json.html != '') {
+                    mdui.$('#afdian_sponsors').html(_json.html);
+                    window.scroll(0, 348);
+                } else {
+                    mdui.alert('没有更多了', '提示');
+                    return;
+                }
+
+            } else {
+                mdui.alert(_json.msg, '出错啦！');
+                return;
+            }
+        },
+        error: function () {
+            mdui.alert('拉取信息时出错', '出错啦！');
+            return;
+        }
+    });
+}
